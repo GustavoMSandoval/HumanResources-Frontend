a39e45d1f7208c81d117557f4d3ccb61d5da2122
GustavoMSandoval <gmsandoval2005@gmail.com>
2026-01-03 20:40:41 -0300

    fix: Arrumando lógica da listagem e adicionando botão de deletar funcional

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,31 @@
+'use client'
+
 import { Employee } from "@/types/employee";
+import { useEffect, useState } from "react";
 
-export default async function Home() {
+export default function Home() {
 
-  let employees: Employee[];
+  const [employees, setEmployees] = useState<Employee[]>([]);
 
-  try {
-    const res = await fetch('http://localhost:8080/api/employees', {
-      cache: 'no-store'
+  useEffect(() => {
+    async function load() {
+      const res = await fetch('http://localhost:8080/api/employees', {
+        cache: 'no-store'
+      });
+      setEmployees(await res.json());
+    }
+
+    load();
+  }, []);
+
+  async function handleDelete(id: number) {
+
+    const res = await fetch(`http://localhost:8080/api/employees/${id}`, {
+      method: 'DELETE'
     });
 
-    employees = await res.json();
-  } catch (e) {
-    employees = []
-    console.log(e)
+    setEmployees(prev => prev.filter(e => e.id !== id));
+
   }
 
   
@@ -34,15 +47,15 @@ export default async function Home() {
           </tr>
         </thead>
         <tbody>
-          {employees.map( employee => (
-          <tr key={employee.id} className="border-b border-gray-100">
+          {employees.map( (employee, id) => (
+          <tr key={id} className="border-b border-gray-100">
             <td className="p-3 text-md text-left">{employee.CPF}</td>
             <td className="p-3 text-md text-left">{employee.email}</td>
             <td className="p-3 text-md text-left">{employee.name}</td>
             <td className="p-3 text-md text-left">{employee.salary}</td>
             <td className="tracking-wide p-3 text-md flex items-center gap-2">
               <a href={`/employees/${employee.id}/edit`} className="text-primary">Editar</a>
-              <a href="#" className="text-red-500">Deletar</a>
+              <button onClick={() => handleDelete(employee.id)} type="submit" className="text-red-500 cursor-pointer">Deletar</button>
             </td>
           </tr>
           ))}
