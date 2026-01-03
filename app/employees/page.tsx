27bc41bf7134145c6763b9c512af4dc32ada9659
GustavoMSandoval <gmsandoval2005@gmail.com>
2026-01-03 19:26:46 -0300

    refactor: Mudando design do formulário

--- app/employees/page.tsx
+++ app/employees/page.tsx
@@ -27,16 +27,20 @@ export default function Create() {
                 <h2 className="mb-8 text-center text-xl font-semibold text-primary">Adicionar novo funcionário</h2>
                 <form action={createEmployee} className="flex flex-col border rounded-2xl border-gray-400 bg-gray-50 p-5">
                     <div className="relative z-0 w-full mb-5 group">
-                        <input value={cpf} onChange={handleChange} className="input-primary" placeholder="CPF" name="cpf" type="text" min={14} maxLength={14}/>
+                        <input id="floating_cpf" value={cpf} onChange={handleChange} className="input-primary peer" placeholder=" " name="cpf" type="text" min={14} maxLength={14}/>
+                        <label htmlFor="floating_cpf" className="text-primary absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">CPF:</label>
                     </div>
                     <div className="relative z-0 w-full mb-5 group">
-                        <input className="input-primary" placeholder="Email" name="email" type="text" />
+                        <input id="floating_email" className="input-primary peer" placeholder=" " name="email" type="text" />
+                        <label htmlFor="floating_email" className="text-primary absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Email:</label>
                     </div>
                     <div className="relative z-0 w-full mb-5 group">
-                        <input className="input-primary" placeholder="Nome" name="name" type="text" />
+                        <input id="floating_name" className="input-primary peer" placeholder=" " name="name" type="text" />
+                        <label htmlFor="floating_name" className="text-primary absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Nome:</label>
                     </div>
                     <div className="relative z-0 w-full mb-5 group">
-                        <input className="input-primary" placeholder="Salário" name="salary" type="number" step={2} />
+                        <input id="floating_salary" className="input-primary peer" placeholder=" " name="salary" type="number" step={2} />
+                        <label htmlFor="floating_salary" className="text-primary absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Salário:</label>
                     </div>
                     
                     <input className="btn-primary" type="submit" value="Enviar"/>
