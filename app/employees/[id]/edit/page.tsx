04b2714fbd584928c82f25dfaf5311ed944a9b2c
GustavoMSandoval <gmsandoval2005@gmail.com>
2026-01-07 08:45:57 -0300

    feat: Adicionando edit page

---
+++ app/employees/[id]/edit/page.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import { editEmployee } from "../../actions";
+import { Employee } from "@/types/employee";
+import { useParams } from "next/navigation";
+
+export default function Edit() {
+
+    const [employee, setEmployee] = useState<Employee>();
+
+    const {id} = useParams();
+
+    try {
+      useEffect(() => {
+        async function load() {
+          const res = await fetch(`http://localhost:8080/api/employees/${id}`, {
+            cache: 'no-store'
+          });
+          setEmployee(await res.json());
+        }
+    
+        load();
+      }, []);
+    } catch(e) {
+      console.log("User not found!")
+    }
+
+  return (
+    <>
+        <div className="max-w-md mx-auto flex flex-col justify-center h-screen">
+            <h2 className="mb-8 text-center text-xl font-semibold text-primary">Editar funcionário</h2>
+            <form action={editEmployee} className="flex flex-col border rounded-2xl border-gray-400 bg-gray-50 p-5">
+                <div className="relative z-0 w-full mb-5 group">
+                    <input id="floating_cpf" className="input-primary" placeholder={employee?.CPF} name="cpf" type="text" min={14} maxLength={14}/>
+                    <label htmlFor="floating_cpf" className="text-primary absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-left peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">CPF:</label>
+                </div>
+                <div className="relative z-0 w-full mb-5 group">
+                    <input id="floating_email" className="input-primary" placeholder={employee?.email} name="email" type="text" />
+                    <label htmlFor="floating_email" className="text-primary absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-left peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Email:</label>
+                </div>
+                <div className="relative z-0 w-full mb-5 group">
+                    <input id="floating_name" className="input-primary" placeholder={employee?.name} name="name" type="text" />
+                    <label htmlFor="floating_name" className="text-primary absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-left peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Nome:</label>
+                </div>
+                <div className="relative z-0 w-full mb-5 group">
+                    <input id="floating_salary" className="input-primary peer" placeholder=" " name="salary" type="number" step={2} />
+                    <label htmlFor="floating_salary" className="text-primary absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-left peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Salário:</label>
+                </div>
+                
+                <input className="btn-primary" type="submit" value="Enviar"/>
+            </form>
+        </div>
+    </>
+  )
+}
--- app/employees/actions.ts
+++ app/employees/actions.ts
@@ -18,3 +18,20 @@ export async function createEmployee(formData: FormData) {
     })
     
 }
+
+export async function editEmployee(formData: FormData) {
+
+    const data = {
+        CPF: formData.get('cpf'),
+        name: formData.get('name'),
+        email: formData.get('email'),
+        salary: formData.get('salary')
+    }
+
+    await fetch('http://localhost:8080/api/employees', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data),
+    })
+    
+}
